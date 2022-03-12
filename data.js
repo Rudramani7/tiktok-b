a7f477d1503d7a88f0ab6b54d9c04220e3d8ce9f
Rudramani7 <rudramani1410@gmail.com>
2022-03-12 09:44:09 +0530

    First commit tiktok-b

---
+++ data.js
@@ -0,0 +1,22 @@
+const data =[
+    {
+        url:'https://beststatusvideo.com/files/download/id/17640',
+        channel:'Rudra',
+        song:'One dance',
+        likes:'12343',
+        messages:'5678',
+        description:'Example description',
+        shares:'500',
+    },
+    {
+        url:'https://sabstatus.com/files/download/id/3233',
+        channel:'Tarak',
+        song:'love nwantiti',
+        likes:'16136',
+        messages:'7317',
+        description:'Example description',
+        shares:'399',
+    },
+];
+
+export default data;
